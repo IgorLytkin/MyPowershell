6faae555b981c575f0eee53ad23975ba8b286678
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-12-06 19:51:03 +0700

    Добавил универсальность

--- go1.ps1
+++ go1.ps1
@@ -2,24 +2,29 @@
 Stop-Process -Name ssh -Force -ErrorAction SilentlyContinue
 
 # Устанавливаем ssh-туннели
-$SecretKey = "D:\Yandex\igor.lytkin.2020\YandexDisk\Singularity\Keys\2023\ed25519\id_ed25519"
-ssh -L 22:localhost:22 -v -i $SecretKey -N -f -l liv singularity.lytkins.ru
-ssh -L 5432:localhost:5432 -v -i $SecretKey -N -f -l liv singularity.lytkins.ru
-ssh -L 5433:localhost:5433 -v -i $SecretKey -N -f -l liv singularity.lytkins.ru
-ssh -L 6379:localhost:6379 -v -i $SecretKey -N -f -l liv singularity.lytkins.ru
-ssh -L 7687:localhost:7687 -v -i $SecretKey -N -f -l liv singularity.lytkins.ru
+if ($env:computername = "IGOR2022") { # Ноутбук HP
+    $SecretKey     = "D:\Yandex\igor.lytkin.2020\YandexDisk\Singularity\Keys\2023\ed25519\id_ed25519"
+    $SecretKeyPpk  = "D:\Yandex\igor.lytkin.2020\YandexDisk\Singularity\Keys\2023\ed25519\id_ed25519.ppk"
+} elseif ($env:computername = "IGOR2023") { # Beelink EQ12
+    $SecretKey     = "C:\Users\igorl\YandexDisk\Singularity\Keys\2023\ed25519\id_ed25519"
+    $SecretKeyPpk  = "C:\Users\igorl\YandexDisk\Singularity\Keys\2023\ed25519\id_ed25519.ppk"
+}
+
+$Ports = 22,5432,5433,6379,7687
+foreach ($i in $Ports) {
+    ssh -L $i:localhost:$i -v -i $SecretKey -N -f -l liv singularity.lytkins.ru
+}
 
 # Загружаем секретный ключ с passphrase в Pageant
-$SecretKeyPpk = "D:\Yandex\igor.lytkin.2020\YandexDisk\Singularity\Keys\2023\ed25519\id_ed25519.ppk"
-
-Start-Process "pageant.exe" -ArgumentList $SecretKeyPpk -PassThru 
-# | Wait-Process
+Start-Process "pageant.exe" -ArgumentList $SecretKeyPpk -PassThru
 
 # Ожидаем некоторое время перед запуском туннелей и putty
 Start-Sleep -Seconds 5
 
 # Запускаем процессы putty.exe
-Start-Process "putty.exe" -ArgumentList "-ssh -i $SecretKeyPpk -l liv -L 22:localhost:22 -m script1.sh" -NoNewWindow
-Start-Process "putty.exe" -ArgumentList "-ssh -i $SecretKeyPpk -l liv -L 22:localhost:22 -m script2.sh" -NoNewWindow
+for ($i = 1, $i -le 2, $i++) {
+    Start-Process "putty.exe" -ArgumentList "-ssh -i $SecretKeyPpk -l liv -L 22:localhost:22 -m script" + $i + ".sh" -NoNewWindow
+}
+
 
 
