e27bfb6e9aa4c2e0d0f9bdedc04e16b4c7960f51
IgorLytkin <igor.lytkin.2020@ya.ru>
2023-12-09 14:19:08 +0700

    Добавил ssh-add

--- go.ps1
+++ go.ps1
@@ -3,10 +3,11 @@
 # Источник: https://winitpro.ru/index.php/2019/10/29/windows-ssh-tunneling/
 
 # Переменные
-$ServerFqdn = 'singularity.lytkins.ru'
-$UbuntuUserName = 'liv'
-$Ports = '22','5432','5433','6379','7687'
+$ServerFqdn = "singularity.lytkins.ru"
+$UbuntuUserName = "liv"
+$Ports = "22","5432","5433","6379","7687"
 $LocalComputerName = $env:ComputerName
+# $PassPhrase = Read-Host -Prompt "SSH Key Passphraze: " -AsSecureString
 
 # Проверяем наличие ssh-клиента на машине с ОС Windows
 Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH.Client*'
@@ -26,27 +27,35 @@ Write-Host 'Имя компьютера:', $LocalComputerName
 Write-Host 'Секретный ssh-ключ:', $SecretKey
 Write-Host 'Секретный ssh-ключ (для pagent):', $SecretKeyPpk
 
+# Добавляем секретный ключ в ssh-agent
+ssh-add -v $SecretKey
+
 # Цикл по номерам портов, создаем ssh-туннель на порт
 Write-Host 'Создаём ssh-туннели для портов ',$Ports
 foreach ($i in $Ports) {
+    Write-Host $i
     $s_Port = $i+":localhost:"+$i
+    Write-Host $s_Port
     $s = "Проброс порта localost :",$i,"->",$ServerFqdn,":",$i
     Write-Host
     Write-Host $s
     Write-Host
     ssh -L $s_Port -v -i $SecretKey -N -f -l $UbuntuUserName  $ServerFqdn
 
-    # TODO: анализ кода возврата ssh
+    # TODO: анализ кода возврата ssh.exe
 
 }
 
 # Загружаем секретный ключ с passphrase в Pageant
 Start-Process "pageant.exe" -ArgumentList $SecretKeyPpk -PassThru
+# TODO: анализ кода возврата pagent.exe
 
 # Ожидаем некоторое время перед запуском туннелей и putty
 Start-Sleep -Seconds 5
 
 # Запускаем процессы putty.exe
-for ($j = 1, $j -le 2, $j++) {
+for ($j = 1;$j -le 2;$j++) {
+    Write-Host  'Запуск Putty #',$j
     Start-Process "putty.exe" -ArgumentList "-ssh -i $SecretKeyPpk -l liv -L 22:localhost:22" -NoNewWindow
+    # TODO: анализ кода возврата putty.exe
 }
